7ead36b588390e9cc113837a4da34b785bf154c8
EmmanuelAdu <christorenzo20@gmail.com>
2024-06-17 13:24:11 +0000

    Express API routing working perfectly

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes/index';
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+export default app;
